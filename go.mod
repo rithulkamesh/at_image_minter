79bdac4ea3260433e99984967fc3f88772657f3a
Rithul Kamesh <hi@rithul.dev>
2024-03-26 12:10:18 +0530

    feat: vip and gen tickets

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.1
 require (
 	github.com/labstack/echo/v4 v4.11.4
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	golang.org/x/image v0.15.0
 )
 
 require (
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"image"
+	"image/color"
 	"image/draw"
 	"image/jpeg"
 	"io/ioutil"
@@ -10,53 +11,97 @@ import (
 
 	"github.com/labstack/echo/v4"
 	qrcode "github.com/skip2/go-qrcode"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/math/fixed"
 )
+
+func generateImage(ticketID string, vip bool) (*bytes.Buffer, error) {
+	baseFile, err := ioutil.ReadFile("ticket_template.jpg")
+	if err != nil {
+		return nil, err
+	}
+
+	baseJpg, err := jpeg.Decode(bytes.NewReader(baseFile))
+	if err != nil {
+		return nil, err
+	}
+
+	qrcodeBytes, err := qrcode.Encode("https://at-ticket-scan.vercel.app/api/qr/"+ticketID, qrcode.Medium, 300)
+	if err != nil {
+		return nil, err
+	}
+
+	qrcodeImg, _, err := image.Decode(bytes.NewReader(qrcodeBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	bounds := baseJpg.Bounds()
+	overlay := image.NewRGBA(bounds)
+	draw.Draw(overlay, bounds, baseJpg, image.ZP, draw.Src)
+
+	draw.Draw(overlay, bounds, qrcodeImg, image.Pt(-1550, -200), draw.Over)
+
+	face := basicfont.Face7x13 // Use a larger font face
+	face.Width = 16
+	drawer := &font.Drawer{
+		Dst:  overlay,
+		Src:  image.NewUniform(color.RGBA{255, 255, 255, 255}),
+		Face: face,
+		Dot:  fixed.Point26_6{X: 1623 * 64, Y: 70 * 64}, // Adjust the coordinates as needed
+	}
+
+	if vip {
+		drawer.DrawString(ticketID + " (VIP)")
+
+	} else {
+		drawer.DrawString(ticketID + " (GEN)")
+
+	}
+
+	buf := new(bytes.Buffer)
+	err = jpeg.Encode(buf, overlay, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf, nil
+}
 
 func main() {
 	e := echo.New()
 
-	e.GET("/ticket/:ticketID", serveTicketImage)
+	e.GET("/ticket/vip/:ticketID", serveTicketImageVIP)
+	e.GET("/ticket/gen/:ticketID", serveTicketImage)
 
 	e.Logger.Fatal(e.Start("0.0.0.0:8080"))
+}
+
+func serveTicketImageVIP(c echo.Context) error {
+	ticketID := c.Param("ticketID")
+
+	// c.Response().Header().Set("Content-Disposition", "attachment; filename="+ticketID+".jpg")
+	// c.Response().Header().Set("Content-Type", "image/jpeg")
+
+	buf, err := generateImage(ticketID, true)
+	if err != nil {
+		return err
+	}
+
+	return c.Blob(http.StatusOK, "image/jpeg", buf.Bytes())
 }
 
 func serveTicketImage(c echo.Context) error {
 	ticketID := c.Param("ticketID")
 
-	baseFile, err := ioutil.ReadFile("ticket_template.jpg")
+	// c.Response().Header().Set("Content-Disposition", "attachment; filename="+ticketID+".jpg")
+	// c.Response().Header().Set("Content-Type", "image/jpeg")
+
+	buf, err := generateImage(ticketID, false)
 	if err != nil {
 		return err
 	}
-
-	baseJpg, err := jpeg.Decode(bytes.NewReader(baseFile))
-	if err != nil {
-		return err
-	}
-
-	qrcodeBytes, err := qrcode.Encode("https://at-ticket-scan.vercel.app/api/qr/"+ticketID, qrcode.Medium, 300)
-	if err != nil {
-		return err
-	}
-
-	qrcodeImg, _, err := image.Decode(bytes.NewReader(qrcodeBytes))
-	if err != nil {
-		return err
-	}
-
-	bounds := baseJpg.Bounds()
-	overlay := image.NewRGBA(bounds)
-
-	draw.Draw(overlay, bounds, baseJpg, image.ZP, draw.Src)
-	draw.Draw(overlay, bounds, qrcodeImg, image.Pt(-1550, -200), draw.Over)
-
-	buf := new(bytes.Buffer)
-	err = jpeg.Encode(buf, overlay, nil)
-	if err != nil {
-		return err
-	}
-
-	c.Response().Header().Set("Content-Disposition", "attachment; filename="+ticketID+".jpg")
-	c.Response().Header().Set("Content-Type", "image/jpeg")
 
 	return c.Blob(http.StatusOK, "image/jpeg", buf.Bytes())
 }
